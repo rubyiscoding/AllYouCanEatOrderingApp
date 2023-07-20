3261cca62796c10b7b3baffe87e0d15572507140
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-07-20 15:06:21 -0400

    Implemented MenuAdapter class for displaying menu items in a RecyclerView with click functionality.

---
+++ app/src/main/java/com/example/allyoucaneatorderingapp/MenuAdapter.kt
@@ -0,0 +1,41 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuAdapter(
+    private val menuItems: List<MenuItem>,
+    private val onItemClick: (MenuItem) -> Unit
+) : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val itemName: TextView = itemView.findViewById(R.id.itemNameTextView)
+        private val itemDescription: TextView = itemView.findViewById(R.id.itemDescriptionTextView)
+        private val itemPhoto: ImageView = itemView.findViewById(R.id.itemPhotoImageView)
+
+        fun bind(menuItem: MenuItem) {
+            itemName.text = menuItem.name
+            itemDescription.text = menuItem.description
+            itemPhoto.setImageResource(menuItem.photoResId)
+
+            itemView.setOnClickListener {
+                onItemClick(menuItem)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.menu_item_layout, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val menuItem = menuItems[position]
+        holder.bind(menuItem)
+    }
+
+    override fun getItemCount(): Int = menuItems.size
+}

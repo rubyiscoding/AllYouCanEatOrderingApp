b6cf716f4ed62f4ffd25c34df917e43d376f60f3
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-07-21 11:33:04 -0400

    Updated deprecated method calls and improved intent data handling

--- app/src/main/java/com/example/allyoucaneatorderingapp/MenuAdapter.kt
+++ app/src/main/java/com/example/allyoucaneatorderingapp/MenuAdapter.kt
@@ -1,3 +1,4 @@
+package com.example.allyoucaneatorderingapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/example/allyoucaneatorderingapp/OrderSummaryActivity.kt
+++ app/src/main/java/com/example/allyoucaneatorderingapp/OrderSummaryActivity.kt
@@ -4,7 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_order_summary.*
+import android.widget.ImageView
+import android.widget.TextView
 
 class OrderSummaryActivity : AppCompatActivity() {
 
@@ -14,31 +15,46 @@ class OrderSummaryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_summary)
 
-        selectedItem = intent.getSerializableExtra("selectedItem") as MenuItem
-        populateOrderSummary(selectedItem)
+        val itemNameTextView = findViewById<TextView>(R.id.itemNameTextView)
+        val itemDescriptionTextView = findViewById<TextView>(R.id.itemDescriptionTextView)
+        val itemPhotoImageView = findViewById<ImageView>(R.id.itemPhotoImageView)
 
-        confirmOrderButton.setOnClickListener {
+        selectedItem = intent.getSerializableExtra("selectedItem") as? MenuItem
+            ?: run {
+                // Handle the case when the selected item is null or not of the correct type
+                finish()
+                return
+            }
+
+        populateOrderSummary(selectedItem, itemNameTextView, itemDescriptionTextView, itemPhotoImageView)
+
+        findViewById<TextView>(R.id.confirmOrderButton).setOnClickListener {
             // Add logic here to handle confirming the order
-            setResult(RESULT_OK)
+            setResult(Activity.RESULT_OK)
             finish()
         }
 
-        changeOrderButton.setOnClickListener {
+        findViewById<TextView>(R.id.changeOrderButton).setOnClickListener {
             // Add logic here to change the order if needed
             val intent = Intent()
             intent.putExtra("changedItem", selectedItem)
-            setResult(RESULT_OK, intent)
+            setResult(Activity.RESULT_OK, intent)
             finish()
         }
 
-        deleteOrderButton.setOnClickListener {
+        findViewById<TextView>(R.id.deleteOrderButton).setOnClickListener {
             // Add logic here to delete the order
-            setResult(RESULT_CANCELED)
+            setResult(Activity.RESULT_CANCELED)
             finish()
         }
     }
 
-    private fun populateOrderSummary(menuItem: MenuItem) {
+    private fun populateOrderSummary(
+        menuItem: MenuItem,
+        itemNameTextView: TextView,
+        itemDescriptionTextView: TextView,
+        itemPhotoImageView: ImageView
+    ) {
         itemNameTextView.text = menuItem.name
         itemDescriptionTextView.text = menuItem.description
         itemPhotoImageView.setImageResource(menuItem.photoResId)

d7cdfe7a0bd81172eca4a1df3e16bbcc0b6b5af7
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-18 17:41:12 -0400

    Added History fragment which is responsible for displaying user order history. (incomplete) Also added Profile Fragment which displayed user profile information and allows users to sign out using google sign in. Layouts for both the fragments have been included.

---
+++ app/src/main/java/com/example/allyoucaneatorderingapp/Fragments/ProfileFragment.kt
@@ -0,0 +1,40 @@
+package com.example.allyoucaneatorderingapp.Fragments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.allyoucaneatorderingapp.LoginActivity
+import com.example.allyoucaneatorderingapp.R
+import com.example.allyoucaneatorderingapp.databinding.FragmentProfileBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding : FragmentProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+
+        binding.signout.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+            GoogleSignIn.getClient(requireContext(),gso).signOut()
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(requireContext(),LoginActivity::class.java))
+        }
+        return binding.root
+    }
+
+    companion object {
+    }
+}

21323be26bc777abc1bbbc9a1a93d2b338dc199d
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-18 11:09:39 -0400

    Implemented Choose Location Screen: Added the ChooseLocationActivity along with its layout XML, enabling users to select a city from a spinner and proceed to the main screen with the chosen city as context.

---
+++ app/src/main/java/com/example/allyoucaneatorderingapp/ChooseLocationActivity.kt
@@ -0,0 +1,48 @@
+package com.example.allyoucaneatorderingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import com.example.allyoucaneatorderingapp.databinding.ActivityChooseLocationBinding
+
+class ChooseLocationActivity : AppCompatActivity() {
+
+    val binding : ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        val windowInsetController = ViewCompat.getWindowInsetsController(window.decorView)
+        windowInsetController?.isAppearanceLightStatusBars = true
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val locationList = arrayOf("Sudbury", "Lively", "Azilda", "Coniston")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+        binding.spinner.setAdapter(adapter)
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (parent != null){
+                    binding.next.setOnClickListener {
+                        val city = parent.selectedItem.toString()
+                        val intent = Intent(this@ChooseLocationActivity,MainActivity::class.java)
+                        intent.putExtra("city",city)
+                        startActivity(intent)
+                    }
+                }
+
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+    }
+}

974a3856eb5f901d28d11705705edc65f3796f77
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-18 18:18:10 -0400

    Implemented MenuActivity class and used MenuAdapter within a Recycler View to display a list of items in the app's menu. activity_main.xml is used to define the UI layout for menu activity.

---
+++ app/src/main/java/com/example/allyoucaneatorderingapp/MenuActivity.kt
@@ -0,0 +1,31 @@
+package com.example.allyoucaneatorderingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.allyoucaneatorderingapp.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        val windowInsetController = ViewCompat.getWindowInsetsController(window.decorView)
+        windowInsetController?.isAppearanceLightStatusBars = true
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener {
+            finish()
+        }
+        val menuFoodName = listOf("Burger","Sandwich","Mo:Mo","Chowmein","Pizza","Shake")
+        val menuItemPrice = listOf("$ 7.85","$ 5.8","$ 10.39","$4.31","$15.92","$4.48")
+        val menuImage = listOf(R.drawable.burger, R.drawable.sandwich,R.drawable.momos,R.drawable.chowmein, R.drawable.pizza,R.drawable.shake)
+        val adapter = MenuAdapter(ArrayList(menuFoodName),ArrayList(menuItemPrice),ArrayList(menuImage), this)
+        binding.menuRecycler.layoutManager = LinearLayoutManager(this)
+        binding.menuRecycler.adapter = adapter
+    }
+}

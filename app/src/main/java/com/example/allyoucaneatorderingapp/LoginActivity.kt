760fcae37abafcde52b5675fb3c0eac470276415
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-08-20 15:13:43 -0400

    Replaced firebase authentication with hardcoded user credentials, due to compatibility issues and constraints within the project. Implemented SharedPreferences to store and retrieve user credentials securely, and updated sign up and login activity.

--- app/src/main/java/com/example/allyoucaneatorderingapp/LoginActivity.kt
+++ app/src/main/java/com/example/allyoucaneatorderingapp/LoginActivity.kt
@@ -1,47 +1,29 @@
 package com.example.allyoucaneatorderingapp
 
-
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import com.example.allyoucaneatorderingapp.databinding.ActivityLoginBinding
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
+
+    // initializing SharedPreferences
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.statusBarColor = ContextCompat.getColor(this, R.color.Primary)
-
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
-        googleSignInClient = GoogleSignIn.getClient(this,gso)
 
-        binding.google.setOnClickListener {
-            val signInClient = googleSignInClient.signInIntent
-            launcher.launch(signInClient)
-        }
 
-        auth = Firebase.auth
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
         binding.goToSignup.setOnClickListener {
-            val intent = Intent(this,SignupActivity::class.java)
+            val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
 
@@ -51,61 +33,33 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.login.setOnClickListener {
-            if (binding.email1.text.toString().length<=1 || binding.password1.text.toString().length<=5){
+            val enteredEmail = binding.email1.text.toString()
+            val enteredPassword = binding.password1.text.toString()
+
+            if (enteredEmail.isEmpty() || enteredPassword.isEmpty()) {
                 Toast.makeText(
                     this,
                     "Please enter complete details.",
-                    Toast.LENGTH_SHORT,
+                    Toast.LENGTH_SHORT
                 ).show()
-            }
-            else{
-                auth.signInWithEmailAndPassword(binding.email1.text.toString(), binding.password1.text.toString())
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            val user = auth.currentUser
-                            startActivity(Intent(this@LoginActivity,MainActivity::class.java))
-                        } else {
-                            Toast.makeText(
-                                this,
-                                "Authentication failed.",
-                                Toast.LENGTH_SHORT,
-                            ).show()
-                        }
-                    }
-            }
-        }
+            } else {
+                // retrieve the saved user credentials from SharedPreferences
+                val savedEmail = sharedPreferences.getString("user_email", "")
+                val savedPassword = sharedPreferences.getString("user_password", "")
 
-    }
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-        if (result.resultCode== RESULT_OK){
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            if (task.isSuccessful){
-                val account: GoogleSignInAccount?=task.result
-                val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
-                auth.signInWithCredential(credential).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        Toast.makeText(this,"Successful",Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this,ChooseLocationActivity::class.java))
-
-                    }
-                    else{
-                        Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
-                    }
+                if (enteredEmail == savedEmail && enteredPassword == savedPassword) {
+                    // Successful login
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Authentication failed. Please check your credentials.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
-        else{
-            Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        if (auth.currentUser != null){
-            startActivity(Intent(this,MainActivity::class.java))
-        }
     }
 }
+
 
--- app/src/main/java/com/example/allyoucaneatorderingapp/SignupActivity.kt
+++ app/src/main/java/com/example/allyoucaneatorderingapp/SignupActivity.kt
@@ -1,100 +1,78 @@
 package com.example.allyoucaneatorderingapp
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.example.allyoucaneatorderingapp.databinding.ActivitySignupBinding
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
+
+    // initializing SharedPreferences
+    private lateinit var sharedPreferences: SharedPreferences
+
+    // i have hardcoded user credentials here for 3 test users
+    private val hardcodedUsers = listOf(
+        User("Test User 1", "user1@email.com", "password1"),
+        User("Test User 2", "user2@email.com", "password2"),
+        User("Test User 3", "user3@email.com", "password3")
+    )
+
+    data class User(val name: String, val email: String, val password: String)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.statusBarColor = ContextCompat.getColor(this, R.color.Primary)
-
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
-        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
-        binding.google.setOnClickListener {
-            val signInClient = googleSignInClient.signInIntent
-            launcher.launch(signInClient)
-        }
-
-        auth = Firebase.auth
         binding.goToLogin.setOnClickListener {
-            val intent = Intent(this@SignupActivity,LoginActivity::class.java)
+            val intent = Intent(this@SignupActivity, LoginActivity::class.java)
             startActivity(intent)
         }
 
         binding.signup.setOnClickListener {
-            if (binding.email.text.toString().length<=1 || binding.password.text.toString().length<=5 || binding.name.text.toString().length==0){
+            val enteredName = binding.name.text.toString()
+            val enteredEmail = binding.email.text.toString()
+            val enteredPassword = binding.password.text.toString()
+
+            if (enteredEmail.isEmpty() || enteredPassword.isEmpty() || enteredName.isEmpty()) {
                 Toast.makeText(
                     this,
-                    "Enter complete details",
-                    Toast.LENGTH_SHORT,
+                    "Please enter complete details",
+                    Toast.LENGTH_SHORT
                 ).show()
-            }
-            else{
-                auth.createUserWithEmailAndPassword(binding.email.text.toString(), binding.password.text.toString())
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            val user = auth.currentUser
-                            Toast.makeText(
-                                this,
-                                "Authentication Successful.",
-                                Toast.LENGTH_SHORT,
-                            ).show()
-                            startActivity(Intent(this@SignupActivity,ChooseLocationActivity::class.java))
-                        } else {
-                            Toast.makeText(
-                                this,
-                                "Please Fill Correct details",
-                                Toast.LENGTH_SHORT,
-                            ).show()
-                        }
-                    }
-            }
-        }
-    }
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
-        if (result.resultCode== Activity.RESULT_OK){
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            if (task.isSuccessful){
-                val account: GoogleSignInAccount?=task.result
-                val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
-                auth.signInWithCredential(credential).addOnCompleteListener {
-                    if (it.isSuccessful){
-                        Toast.makeText(this,"Successful",Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this,ChooseLocationActivity::class.java))
+            } else {
+                // checking if the entered email is in the list of hardcoded users
+                val matchedUser = hardcodedUsers.find { it.email == enteredEmail }
 
-                    }
-                    else{
-                        Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
-                    }
+                if (matchedUser != null && matchedUser.password == enteredPassword) {
+                    // saving user credentials to SharedPreferences
+                    val editor = sharedPreferences.edit()
+                    editor.putString("user_email", enteredEmail)
+                    editor.putString("user_password", enteredPassword)
+                    editor.apply()
+
+                    Toast.makeText(
+                        this,
+                        "Registration Successful.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    startActivity(Intent(this@SignupActivity, ChooseLocationActivity::class.java))
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Invalid credentials. Please check your details.",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
-        else{
-            Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
-        }
     }
-
 }
+
